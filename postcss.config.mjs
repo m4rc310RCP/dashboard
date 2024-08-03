dd3b6adede13c4de1d9c5977a3b8d54f961a97f5
m4rc310 <mlsilva@coamo.com.br>
2024-08-03 16:29:28 -0300

    add tailwindcss

---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+// postcss.config.mjs
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Nova versão da Aplicação - V1 -- 3.0.0 ------ Bla-bla-bla
-      </p>
+		<div className="flex p-1">
+      <h1 className="text-xs text-white bg-red-600 px-2 py-1 rounded-md">App Dashboard</h1>
+
+		</div>
     </>
   )
 }
